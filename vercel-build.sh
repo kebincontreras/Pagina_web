3fc927589f9011cec707672399ac5f79f533d8d5
kebincontreras <kebincontreras@gmail.com>
2025-10-08 19:48:59 -0500

    Update vercel-build.sh with current Node.js, Go, and Hugo versions

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # default versions
-NODE_VERSION='18.16.1';
-GO_VERSION='1.22.2';
-HUGO_VERSION='0.124.1';
+NODE_VERSION='22.19.0';
+GO_VERSION='1.23.3';
+HUGO_VERSION='0.139.0';
 
 # install Node.js
 # echo "Installing Node.js $NODE_VERSION..."
